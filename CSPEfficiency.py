3142f29b9a3bc278764423aa8f8a0f6fd304e3fe
EliseDup <Elise@mac-1o0-339.local>
2018-06-07 16:55:32 +0200

    CSP efficiency interpolation and EROI optimisation

--- CSPEfficiency.py
+++ CSPEfficiency.py
@@ -2,53 +2,52 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
+import scipy as sp
+import scipy.interpolate
 
 def main():
-    dni_efficiency("../trough",3)
-  
-def dni_efficiency(file, degree):
-    data = genfromtxt(file, delimiter='\t', dtype=None)
     
-    x = data[:, 0]; y = data[:, 1] 
-    z = np.poly1d(np.polyfit(x, y, degree))
-    print z
-    xp = np.linspace(500, 3000, 100)
-   
-    plt.figure; plt.plot(x,y,'.',xp,z(xp))
-    plt.xlabel("DNI")
-    plt.ylabel("Efficiency [%]")
-    plt.title(file)
+    sm = np.linspace(0.1, 5, 100)
+    plt.figure; plt.plot(sm, efficiency_trough0h(2000,sm)); plt.show()
+#     data_dni = genfromtxt("../sam_data/dni_eff_trough0h", delimiter='\t', dtype=None)
+#     data_sm = genfromtxt("../sam_data/sm_norm_efficiency_trough0h", delimiter='\t', dtype=None)
+#     dni_eff = ln_interp1d(data_dni[:,0], data_dni[:,1],  np.linspace(500, 5000, 100),False)
+#     sm_eff = interp1d(data_sm[:,0], data_sm[:,1],  np.linspace(0, 5, 100), 5,False)
+#     x = np.linspace(0.1, 5, 100)
+#     y1000 = dni_eff(np.log(1000))*sm_eff(x)
+#     y2000 = dni_eff(np.log(2000))*sm_eff(x)
+#     y3000 = dni_eff(np.log(3000))*sm_eff(x)
+#     plt.figure; plt.plot(x,y1000,x,y2000,x,y3000); plt.show()
     
-    plt.show()
     
-    return z
-    
-def troughNoStorage(degree):
-    x = np.array([0.5, 0.75, 1.0, 1.25, 1.3, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3, 0.5, 0.75, 1.0, 1.25, 1.3, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3, 0.5, 0.75, 1.0, 1.25, 1.3, 1.5, 1.75, 2.0, 2.25, 2.5, 2.75, 3]) 
-    y = np.array([8.89,11.88,13.04,13.44,13.44,13.00,12.17,11.25,10.45,9.74,9.11,8.51,10.73,13.35,14.36,14.76,14.62,13.90,12.77,11.65 ,10.71,9.88,9.18 ,8.54,11.06,12.85,13.61,13.77,13.52,12.62,11.49,10.42,9.53,8.74,8.09,7.50])
-    xp = np.linspace(0, 3, 100)
-    z = np.poly1d(np.polyfit(x, y, degree))
-    plt.figure; plt.plot(x,y,'.',xp,z(xp))
-    plt.xlabel("Solar Multiple")
-    plt.ylabel("Efficiency [%]")
-    plt.title("Parabolic Trough No Storage")
-    plt.show()
-    
+def efficiency_trough0h(dni, sm):  
+    data_dni = genfromtxt("../sam_data/dni_eff_trough0h", delimiter='\t', dtype=None)
+    data_sm = genfromtxt("../sam_data/sm_norm_efficiency_trough0h", delimiter='\t', dtype=None)
+    dni_eff = np.poly1d(np.polyfit(np.log(data_dni[:,0]), data_dni[:,1], 1))
+    sm_eff = np.poly1d(np.polyfit(data_sm[:,0], data_sm[:,1],4))
+    return dni_eff(np.log(dni))*sm_eff(sm)/100.0
+
+def efficiency_trough12h(dni, sm):  
+    data_dni = genfromtxt("../sam_data/dni_eff_trough12h", delimiter='\t', dtype=None)
+    data_sm = genfromtxt("../sam_data/sm_norm_efficiency_trough12h", delimiter='\t', dtype=None)
+    dni_eff = np.poly1d(np.polyfit(np.log(data_dni[:,0]), data_dni[:,1], 1))
+    sm_eff = np.poly1d(np.polyfit(data_sm[:,0], data_sm[:,1],4))
+    return dni_eff(np.log(dni))*sm_eff(sm)/100.0
+
+def ln_interp1d(x, y, xp, plot):
+    logx = np.log(x)
+    z = np.poly1d(np.polyfit(logx, y, 1))
+    print 'interpolation a ln(x) + b:', z
+    if (plot):
+        plt.figure; plt.plot(x,y,'.',xp,z(np.log(xp))); plt.show()
     return z
 
-def trough12HoursStorage(degree):
-    x = np.array([1,1.5,2,2.25,2.5,2.75,3,3.25,3.5,4,4.5,5,5.5,6,1,1.5,2,2.25,2.5,2.75,3,3.25,3.5,4,4.5,5,5.5,6,1,1.5,2,2.25,2.5,2.75,3,3.25,3.5,4,4.5,5,5.5,6]) 
-    y = np.array([13.6738966164829,    14.187516819295,    14.6871832728589,    14.8352319519263,    14.7666836309427,    14.2837019403469,    13.7254867918645,    13.1756947422504,    12.5990100894301,    11.5542739186743,    10.6244989911952,    9.76724234365377,    9.00941347400859,    8.32275935700974,    13.0278914038241,    13.6708568985666,    14.1222468262367,    14.2942642005532,    14.455143282423,    14.5774844217982,    14.3309882000552,    13.9908434771544,    13.5790083108125,    12.6702348500374,    11.8096011241008,    10.9584950164815,    10.216532855903,    9.50302033008429,    14.4628398198745,    15.0858219533938,    15.6627371688348,    15.856220514364,    16.0088182696013,    15.9564400965044,    15.6754217674983,    15.2634606050314,    14.7132499857261,    13.5619262694737,    12.4350487255124,    11.3993005612062,    10.4916338698931,    9.67573876127865,])
-    xp = np.linspace(0, 6, 100)
+def interp1d(x, y, xp, degree, plot):
     z = np.poly1d(np.polyfit(x, y, degree))
-    plt.figure; plt.plot(x,y,'.',xp,z(xp))
-    plt.xlabel("Solar Multiple")
-    plt.ylabel("Efficiency [%]")
-    plt.title("Parabolic Trough 12 Hours Storage")
-    plt.show()
-    
+    print 'interpolation', degree, ':', z
+    if (plot):
+        plt.figure; plt.plot(x,y,'.',xp,z(xp)); plt.show()
     return z
-
 
 if __name__ == "__main__":
     sys.exit(main())
--- CSPOptimisation.py
+++ CSPOptimisation.py
@@ -1,23 +1,10 @@
 import sys
 import numpy as np
 from scipy.optimize import minimize, root
+from CSPEfficiency import efficiency_trough0h, efficiency_trough12h
+from numpy import genfromtxt
+import matplotlib.pyplot as plt
 
-def main():
-    dni = 400*8.76
-    print optimizeSolarMultiple(dni, 2922368.611, 4487437.377, 0.1, default_area(1))
-    print "Hello"
-
-def default_area(powerMW):
-    return powerMW*1E6/(950*0.15)
-
-def fullLoadHours(dni, sm):
-    return (2.5715*dni-694)*(-0.0371*sm*sm+0.4171*sm-0.0744)
-
-def efficiency(sm):
-    return (-0.8522*sm*sm*sm*sm+8.115*sm*sm*sm-27.56*sm*sm+36.01*sm-1.9)/100.0
-
-def efficiency(dni):
-    return (4.318e-10*dni*dni*dni - 5.991e-06*dni*dni + 0.01982*dni - 4.291)/100.0
 # Calculate Solar Multiple that maximizes the EROI
 # With : 
 # - DNI : direct normal irradiance in kWh/m2/year
@@ -25,17 +12,88 @@ def efficiency(dni):
 # - ee_fixed : fixed part of the embodied energy (linked to the power block [kWh]
 # - ee_area : part of the embodied energy varying with the aperture area [kWh]
 # - default_area : default aperture area [m2]
+
+# For the 3 technologies tested :
+#EE_Fixed[kWh/MW]    EE_Variable[kWh/area for 1MW]    EE_output[GJ/GJ]
+#2921710.2777777775    2664706.018702423    0.1
+#5455141.666666667    1193483.7107424117    0.07300000000000001
+#7208229.166666666    1996433.3375287117    0.07300000000000001
+#
+_12h = False
+
+ee_fixed_trough0h = 2921710.2777777775   
+ee_area_trough0h = 2664706.018702423
+ee_output_trough0h = 0.1
+ee_fixed_trough12h = 5455141.666666667  
+ee_area_trough12h = 1193483.7107424117
+ee_output_trough12h = 0.073
+ee_fixed_power12h = 7208229.166666666
+ee_area_power12h = 1996433.3375287117
+ee_output_power12h = 0.073
+
+def efficiency(dni, sm):
+    if _12h: return efficiency_trough12h(dni,sm)
+    else: return efficiency_trough0h(dni,sm)
+
+def eroi(dni, sm):
+    eff = efficiency(dni,sm)
+    prod = 30 * dni * eff * sm * default_area(1)
+    if _12h:
+        ee_fixed = ee_fixed_trough12h; ee_area = ee_area_trough12h; ee_prod = ee_output_trough12h;
+    else:
+        ee_fixed = ee_fixed_trough0h; ee_area = ee_area_trough0h; ee_prod = ee_output_trough0h;
+    return prod / (ee_fixed + ee_area * sm + prod * ee_prod)
+
+def default_area(powerMW):
+    return powerMW*1E6/(950*0.22)
+
+def main():
+#     x = np.linspace(0.1, 6, 100)
+#     plt.plot(x, eroi(1500,x), label='1500 kWh/m2/y')
+#     plt.plot(x, eroi(2000,x), label='2000 kWh/m2/y')
+#     plt.plot(x, eroi(2500,x), label='2500 kWh/m2/y')
+#     plt.plot(x, eroi(3000,x), label='3000 kWh/m2/y')
+#     plt.plot(x, eroi(3500,x), label='3500 kWh/m2/y')
+#    
+#     plt.legend()
+#     plt.show()
+    
+    data = genfromtxt('../WindPotentialScala/data_csp_optimisation', delimiter='\t', dtype=None)
+    output = open('res_through_0h', 'w')
+    lats = data[:, 0]; lon = data[:, 1]; dni = data[:, 2];
+   
+    n = len(lats)
+    for i in range(0, n):
+        if i%1000==0: print 'Progress :',i,'/',n
+        res = maximiseEROI(dni[i])
+        output.write(str(lats[i]) + '\t' + str(lon[i]) + '\t' +  str(dni[i]) + '\t'+ str(res[0]) + '\t' + str(res[1]) + '\t' + str(efficiency_trough12h(dni[i],res[0])) + '\n')
      
-def optimizeSolarMultiple(dni, ee_fixed, ee_area, ee_prod, default_area):
+    output.close()
+     
+    print "End"
+
+def maximiseEROI(dni):
+    def inv_eroi(x):
+        return -eroi(dni, x)
+    res = minimize(inv_eroi, x0=1, bounds=[(1, 6)]) #, options={'disp': True})
     
-    def eroi(x):
-        prod = 30.0 * dni * efficiency(dni) * x * default_area 
-        return - prod / (ee_fixed + ee_area * x + prod * ee_prod)
-        #return - 30.0 * fullLoadHours(dni, x) * 1E3 / (ee_fixed + ee_area * x)
-    res = minimize(eroi, x0=8, bounds=[(1.0, 4.0)]) #, options={'disp': True})
+    return (res.x[0], -inv_eroi(res.x[0]))
+
+def maximiseEfficiency(dni, ee_fixed, ee_area, ee_prod, default_area):
+    def inv_eff(x):
+        return -efficiency(dni,x)
+    
+    res = minimize(inv_eff, x0=1, bounds=[(1, 6)]) #, options={'disp': True})
     return (res.x[0], -eroi(res.x[0]))
- 
-    
+
 if __name__ == "__main__":
     sys.exit(main())
  
+# def fullLoadHours(dni, sm):
+#     return (2.5715*dni-694)*(-0.0371*sm*sm+0.4171*sm-0.0744)
+# 
+# def efficiency(sm):
+#     return (-0.8522*sm*sm*sm*sm+8.115*sm*sm*sm-27.56*sm*sm+36.01*sm-1.9)/100.0
+# 
+# def efficiency(dni):
+#     return (4.318e-10*dni*dni*dni - 5.991e-06*dni*dni + 0.01982*dni - 4.291)/100.0
